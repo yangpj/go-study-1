524cc09f8ae8ace75941d50c584dbb3c694cbae2
yiqing95 <yqiing_95@qq.com>
2013-08-14 13:28:36 +0800

    接口测试2

--- interface/app.go
+++ interface/app.go
@@ -26,4 +26,62 @@ func main() {
 	a = PhoneConnector{"PhoneConnector"}
 	a.Connect()
 
+	Disconnect(a)
+	Disconnect2(a)
+	typeSwitchTest(a)
+
+	typeCastTest()
+
+	emptyInterfaceTest2()
+}
+
+func Disconnect(u USB) {
+	if pc, ok := u.(PhoneConnector); ok {
+		fmt.Println("Disconnected : by PhoneConnector", pc.name)
+	}
+
+}
+
+// ok pattern
+func Disconnect2(u interface{}) {
+	if pc, ok := u.(PhoneConnector); ok {
+		fmt.Println("Disconnected : by PhoneConnector", pc.name)
+	}
+
+}
+
+func typeSwitchTest(obj interface{}) {
+
+	switch v := obj.(type) {
+	case PhoneConnector:
+		fmt.Println("the type is PhoneConnector", v.name)
+	default:
+		fmt.Println("unknown object type")
+	}
+}
+
+type Connector interface {
+	Connect()
+}
+
+type USB2 interface {
+	Name() string
+	//  嵌入接口
+	Connector
+}
+
+type EmptyInterface interface {
+}
+
+func typeCastTest() {
+	pc := PhoneConnector{name: "jj"}
+	c := Connector(pc)
+
+	c.Connect()
+}
+
+func emptyInterfaceTest2() {
+	var a interface{}
+
+	fmt.Println(a == nil)
 }
