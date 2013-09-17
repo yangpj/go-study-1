db4bf5d227ce11a996699397eb33f3a14a886d5f
yiqing-95 <yiqing95@yahoo.com.cn>
2013-09-18 03:10:58 +0800

    读书笔记 theWayToGo

--- helloword/app.go
+++ helloword/app.go
@@ -5,4 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 
+	if 107.17 == (98.6 + 8.57) {
+		fmt.Println("equal !")
+	} else {
+		fmt.Println("not equal!")
+	}
+
+    s := 98.6 + 8.57
+    if 107.17 == s {
+		fmt.Println("equal !")
+	} else {
+		fmt.Println("not equal!")
+	} 
+
 }
